c61bbefb848138041b1d9dc1f644132dc539a94d
ullah <sheikholeei97@gmail.ocm>
2022-11-28 23:09:48 +0600

    project done

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -9,7 +9,7 @@ const Category = () => {
     const { data: categories, isLoading, isError, error } = useQuery({
         queryKey: ['category'],
         queryFn: async () => {
-            const res = await axios.get(`http://localhost:5000/category`);
+            const res = await axios.get(`https://server-tawny-theta.vercel.app/category`);
             return res.data;
         }
 
